96a0403a76a80ccbc23f8a9f2f2550b093c6f764
Lakshita Sharma <24bda70032@cuchd.in>
2026-09-23 16:37:34 +0530

    Time: 0 ms (100%), Space: 11.8 MB (81.2%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       string ans = "";
+for(int i=0;i<strs[0].length();i++){
+    for(int j=0;j<strs.size();j++){
+        if(strs[j][i] != strs[0][i]){
+            return ans;
+        }
+       
+    }
+     ans += strs[0][i];
+}
+return ans;
+        
+    }
+};
